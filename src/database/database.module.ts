0ba37f80beddd03f210056222b196124ac029e67
cristian stiven p <styven21121@gmail.com>
2021-07-07 23:50:48 -0500

    project configuration

---
+++ src/database/database.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { CONFIG } from '../config-keys';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get(CONFIG.DB_URI),
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
